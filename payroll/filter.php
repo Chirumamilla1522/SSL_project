c43c67ba330a2f90fa4cb0d87930ddefef352322
chirumamilla bhargav <84395665+Chirumamilla1522@users.noreply.github.com>
2021-11-10 00:38:26 +0530

    Add files via upload

---
+++ payroll/filter.php
@@ -0,0 +1,157 @@
+<?php
+    session_start();
+    require_once "authenticate/login.php";
+    if($_SESSION['loggedIn'] == true && ($_SESSION['isAdmin'] == true || $_SESSION['isHr'] == true)){
+        // do nothing
+    }
+    else{
+        // echo "<h2> You need to login First </h2>.";
+        echo "<script>alert('You need to login First.')</script>";
+        header('Refresh:01; url=index.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UserDetails</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">Home</a>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="https://www.youtube.com/watch?v=gq-hnrCn8Ms">Video <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <?php
+                        if($_SESSION['loggedIn'] == true)
+                            echo '<a class="nav-link" href="user/logout.php">Logout</a>';
+                        else
+                            echo '<a class="nav-link" href="index.php">Logout</a>';
+                    ?>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin/adminProfile.php">Profile</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0" action="filter.php">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Filter</button>
+            </form>
+        </div>
+    </nav>
+
+    <?php
+        $conn = mysqli_connect($hostname, $username, $password, $database);
+        if(!$conn){
+            die("Error connecting to server. Please try after sometime.".mysqli_connect_error());
+            header('url=filter.php');
+            exit();
+        }
+        else{
+            echo <<< _END
+            
+                <div class="d-flex justify-content-center">
+                    <form action="filter.php" method="POST" >
+                        <div class='form-group'>
+                            <label for='queryUser' style='display: block; text-align: center;'>User Name</label>
+                            <input type="text" name="queriedUser" id='queryUser' class='form-control border-primary'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='queryDate' style='display: block; text-align: center;'>Date</label>
+                            <input type="date" name="queriedDate" id='queryDate' class='form-control border-primary'>
+                        </div>
+                        <div class="col text-center">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+                
+
+_END;
+
+            if(isset($_POST['queriedUser']) && isset($_POST['queriedDate']) && !empty($_POST['queriedDate']) && !empty($_POST['queriedUser'])){
+                $show_all_emp_query = "SELECT username FROM auth WHERE isAdmin='no' AND isHr='no' AND username='{$_POST['queriedUser']}'";
+                $show_all_emp_result = mysqli_query($conn, $show_all_emp_query);
+
+                $number_of_emp = mysqli_num_rows($show_all_emp_result);
+                echo "<div class='d-flex justify-content-center'><div class='col-md-4'>";
+                echo "<h5 style='text-align: center; margin-top: 50px;'>Date: <span class='counter-count'>{$_POST['queriedDate']}</span></h5><br>";
+
+                echo "<div class='list-group'>";
+                $cur_emp_details = mysqli_fetch_row($show_all_emp_result);
+                $present_or_not = "SELECT * FROM {$cur_emp_details[0]} WHERE date_='{$_POST['queriedDate']}'";
+                $result = mysqli_query($conn, $present_or_not);
+                if($result)
+                    $number = mysqli_num_rows($result);
+                if($number_of_emp==0)
+                    echo "<h6 style='text-align: center;'> No employee exists with name {$_POST['queriedUser']}";
+                else if($number > 0)
+                    echo "<a class='list-group-item list-group-item-action border-primary' href='admin/displayUserStats.php?user={$cur_emp_details[0]}&date={$_POST['queriedDate']}' >{$cur_emp_details[0]}</a><br> ";
+                else echo "<h6> {$cur_emp_details[0]} was not present that day </h6><br>";
+
+                echo "</div></div></div>";
+            }
+            else if(isset($_POST['queriedDate']) && !empty($_POST['queriedDate'])){
+                $select_user_tables = "SELECT username FROM auth WHERE isAdmin='no' AND isHr='no'";
+                $selected_user_tables = mysqli_query($conn, $select_user_tables);
+
+                $number_of_emps = mysqli_num_rows($selected_user_tables);
+                echo "<div class='d-flex justify-content-center'><div class='col-md-4'>";
+                echo "<h5 style='text-align: center; margin-top: 50px;'>Date: <span class='counter-count'>{$_POST['queriedDate']}</span></h5><br>";
+
+                echo "<div class='list-group'>";
+
+                for($i=0; $i<$number_of_emps; $i++) {
+                    $curr_emp = mysqli_fetch_row($selected_user_tables);
+                    $present_emp = "SELECT * FROM {$curr_emp[0]} WHERE date_='{$_POST['queriedDate']}'";
+                    $result_emp = mysqli_query($conn, $present_emp);
+                    $present_or_not = mysqli_num_rows($result_emp);
+                    if($present_or_not > 0){
+                        echo "<a class='list-group-item list-group-item-action border-primary' href='admin/displayUserStats.php?user={$curr_emp[0]}&date={$_POST['queriedDate']}' >{$curr_emp[0]}</a><br> ";
+                    }
+                }
+
+                echo "</div></div></div>";
+            }
+            else if(isset($_POST['queriedUser']) && isset($_POST['queriedDate'])) {
+                $show_all_emp_query = "SELECT * FROM auth WHERE isAdmin='no' AND isHr='no' AND username LIKE '%{$_POST['queriedUser']}%'";
+                $show_all_emp_result = mysqli_query($conn, $show_all_emp_query);
+
+                $number_of_emp = mysqli_num_rows($show_all_emp_result);
+                echo "<div class='d-flex justify-content-center'><div class='col-md-4'>";
+                echo "<h5 style='text-align: center; margin-top: 50px;'>Number of employees : <span class='counter-count'>{$number_of_emp}</span> </h5><br>";
+
+                echo "<div class='list-group'>";
+                for($i=0;$i<$number_of_emp;$i+=1){
+                    $cur_emp_details = mysqli_fetch_row($show_all_emp_result);
+                    echo "<a class='list-group-item list-group-item-action border-primary' href='admin/displayUserStats.php?user={$cur_emp_details[0]}' >{$cur_emp_details[0]}</a><br> ";
+                }
+
+                echo "</div></div></div>";
+            }
+        }
+
+    ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ payroll/index.php
@@ -0,0 +1,138 @@
+<?php
+	session_start();
+	require_once "authenticate/login.php";
+
+	// If this page was requested for first time, username and password in html form won't be set
+	if(!isset($_POST['username']) || !isset($_POST['pass'])){
+		// session_destroy();
+		$link = 0;
+		if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true)
+			$link = 'loggedIn';
+		else
+			$link = 'logedOut';
+		echo <<< _END
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+			<link rel="stylesheet" href="index.css">
+
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <a class="navbar-brand" href="index.html">Home</a>
+
+			  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+			    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="https://www.youtube.com/watch?v=gq-hnrCn8Ms">video <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">{$link}</a>
+			      </li>
+			      <li class="nav-item">
+			        
+			      </li>
+			    </ul>
+			  </div>
+			</nav>
+
+			<!-- =====================input form================== -->
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-2">
+						<form method='POST' action='' enctype='multipart/form-data'>
+							<div class="form-group row-md-2">    
+								Username: <input type='text' name='username' class="form-control" placeholder="username" required>
+								<br>
+								Password: <input type='password' name='pass' class="form-control" placeholder="password" required>
+							</div>
+								<input type='submit' class="btn btn-primary">
+						</form>
+					</div>
+					<div class="col-md-3">
+						<div class="thecard">
+							<div class="thecard__side thecard__side--front">
+								<img class="img-fluid" src="images/front3.jpg"></img>
+							</div>
+							<div class="thecard__side thecard__side--back">
+								<img class="img-fluid" src="images/Picture3.jpg">
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thecard">
+							<div class="thecard__side thecard__side--front">
+								<img class="img-fluid" src="images/front.jpg"></img>
+							</div>
+							<div class="thecard__side thecard__side--back">
+								<img class="img-fluid" src="images/Picture4.jpg">
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="thecard">
+							<div class="thecard__side thecard__side--front">
+								<img class="img-fluid" src="images/front2.jpg"></img>
+							</div>
+							<div class="thecard__side thecard__side--back">
+								<img class="img-fluid" src="images/Picture5.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+_END;
+	}
+
+	else{
+		$conn = mysqli_connect($hostname, $username, $password, $database);
+		if(!$conn)
+			die("Error while connectine. Try later. <br>".mysqli_connect_error());
+
+		$currentUserName = trim($_POST['username']);
+		$currentUserPass = trim($_POST['pass']);
+
+		$user_query = "SELECT * FROM auth WHERE username='{$currentUserName}' AND pass='{$currentUserPass}'";
+		$user_result = mysqli_query($conn, $user_query);
+
+		if(!$user_result){
+			die("Error matching credentials. Please try later.<br>".mysqli_error($conn));
+			header('Refresh:01; url="index.php"');
+
+		}
+		else if(mysqli_num_rows($user_result) == 0){
+			echo '<script>alert("Username and password doesn\'t match")</script>';
+			header('Refresh:01; url="index.php"');
+		}
+		else{
+			$_SESSION['user'] = $currentUserName;
+			$_SESSION['loggedIn'] = true;
+			$_SESSION['isAdmin'] = false;
+			$_SESSION['isHr'] = false;
+			echo '<script>alert("Succesfully Logged In. Click ok to go to profile page.")</script>';
+			$data = mysqli_fetch_row($user_result);
+			$isAdmin = $data[2];
+			$isHr = $data[3];
+			
+			if($isAdmin == "yes"){
+				$_SESSION['isAdmin'] = true;
+				header('Refresh:01; url=admin/adminProfile.php');
+				exit();
+			}
+			else if($isHr == "yes"){
+				$_SESSION['isHr'] = true;
+				header('Refresh:01; url=admin/adminProfile.php');
+				exit();
+			}
+			else {
+				$_SESSION['isAdmin'] = false;
+				$_SESSION['isHr'] = false;
+				header('Refresh:01; url=user/userLoginImage.php');
+				exit();
+			}
+		}
+
+	}
+?>
